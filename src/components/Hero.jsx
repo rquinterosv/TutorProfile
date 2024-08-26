4fef52bfe8deea6c1fdd689a2c92da7032cfd29c
rquinterosv <rafael.quinterosv@gmail.com>
2024-08-27 08:45:35 +1200

    Primera propuesta de Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Hero = () => {
+  const { t } = useTranslation();
+
   return (
-    <div className="relative flex flex-col items-center max-w-screen-xl px-4 mx-auto md:flex-row sm:px-6 p-8">
+    <div className="relative flex flex-col items-center max-w-screen-xl px-4 mx-auto md:flex-row sm:px-6 p-2">
       <div className="flex items-center py-5 md:w-1/2 md:pb-20 md:pt-10 md:pr-10">
-        <div className="text-left">
-          <h2 className="text-4xl font-extrabold leading-10 tracking-tight text-gray-800 sm:text-5xl sm:leading-none md:text-6xl">
-            Hero
-            <span className="font-bold text-blue-500">Section</span>
-            <span className="text-xl font-semibold rounded-full text-blueGray-500">2.0</span>
+        <div className="text-center md:text-left">
+          <h2 className="text-6xl font-extrabold leading-10 tracking-tight text-gray-800 sm:text-7xl sm:leading-none md:text-7xl">
+          <span className="font-bold text-blue-500 block sm:inline p-2">Tech</span>
+            Mentor
           </h2>
           <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vulputate dignissim augue, Nullam vulputate dignissim augue.
+          {t('home.mentor_profile')}
           </p>
-          <div className="mt-5 sm:flex md:mt-8">
+          <div className="mt-5 sm:flex md:mt-8 justify-center md:justify-start">
             <div className="rounded-md shadow">
               <a
                 href="#"
                 className="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue md:py-4 md:text-lg md:px-10"
               >
-                Getting started
-              </a>
+                {t('home.get_started')}
+                </a>
             </div>
             <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
               <a
                 href="#"
                 className="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-blue-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-blue-600 focus:outline-none focus:shadow-outline-blue md:py-4 md:text-lg md:px-10"
               >
-                Contribute
-              </a>
+                {t('home.contribute')}
+                </a>
             </div>
           </div>
         </div>
