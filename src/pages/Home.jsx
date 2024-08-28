e0cd3386a643d91686ea0dc3582d2b51a1ad518a
rquinterosv <rafael.quinterosv@gmail.com>
2024-08-28 12:13:38 +1200

    solucionando problemas de lenguaje

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import LengSelect from '../components/LengSelect';
+import React from 'react';
 import Hero from '../components/Hero'
 import Courses from '../components/Courses'
 import Reference from '../components/Reference'
 
 const Home = () => {
-  const [isModalOpen, setIsModalOpen] = useState(true);
-
-  useEffect(() => {
-    const storedLanguage = localStorage.getItem('language');
-    if (storedLanguage) {
-      setIsModalOpen(false); // Si ya hay un idioma guardado, no mostrar el modal
-    }
-  }, []);
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
 
   return (
     <div>
-      {isModalOpen && <LengSelect onClose={handleCloseModal} />}
       <Hero />
       <Courses />
       <Reference />
