8e7ef322523689da17eed09861894a677b98eff0
rquinterosv <rafael.quinterosv@gmail.com>
2024-08-29 17:16:06 +1200

    Arreglando referencias

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -5,18 +5,25 @@ import { faHtml5, faCss3Alt, faJs, faGithub, faBootstrap, faMicrosoft, faJira, f
 import codeImage from "../assets/img/code.jpg";
 import computerImage from "../assets/img/computer.jpg";
 import meetingImage from "../assets/img/meeting.jpg";
+import { Link } from 'react-router-dom';
 
-const InformationCard = ({ image, title, details, logos }) => {
+
+const InformationCard = ({ image, title, details, logos, buttonText }) => {
   return (
     <div className="flex flex-col lg:flex-row w-full mb-8 bg-white rounded-lg shadow-lg overflow-hidden">
       <img src={image} alt={title} className="w-full lg:w-1/2 object-cover" />
       <div className="p-6 flex flex-col justify-center lg:w-1/2">
         <h3 className="text-2xl font-semibold text-blue-500 mb-4">{title}</h3>
         <p className="text-gray-700 mb-6">{details}</p>
-        <div className="flex space-x-4">
+        <div className="flex space-x-4 mb-4">
           {logos.map((logo, index) => (
             <FontAwesomeIcon key={index} icon={logo} size="2x" className="text-blue-500" />
           ))}
+        </div>
+        <div className="mt-8">
+          <Link to="/contact" className="px-6 py-3 text-lg font-semibold text-white bg-blue-500 hover:bg-blue-600 rounded-md">
+            {buttonText}
+          </Link>
         </div>
       </div>
     </div>
@@ -32,18 +39,21 @@ const Information = () => {
       title: t("services.information.web_development"),
       details: t("services.information.web_details"),
       logos: [faHtml5, faCss3Alt, faJs, faGithub, faBootstrap],
+      buttonText: t("contact.get_in_touch"),
     },
     {
       image: computerImage,
       title: t("services.information.microsoft"),
       details: t("services.information.microsoft_details"),
       logos: [faMicrosoft],
+      buttonText: t("contact.get_in_touch"),
     },
     {
       image: meetingImage,
       title: t("services.information.softwares"),
       details: t("services.information.softwares_details"),
       logos: [faJira, faHubspot],
+      buttonText: t("contact.get_in_touch"),
     },
   ];
 
--- src/components/Reference.jsx
+++ src/components/Reference.jsx
@@ -6,6 +6,7 @@ import 'slick-carousel/slick/slick-theme.css';
 import person1 from '../assets/img/andres.jpeg';
 import person2 from '../assets/img/ivan.jpeg';
 import person3 from '../assets/img/ale.jpeg';
+import person4 from '../assets/img/pipe.jpeg';
 
 const references = [
   {
@@ -13,7 +14,7 @@ const references = [
     photo: person1,
     name: 'Andres Gallardo',
     position: 'General Manager at Desafío Latam',
-    description: "We count on Rafa commitment and leadership, which helped us in the creation of different businesses and services that we are still running today in Desafío Latam. I would like to highlight his dynamism and responsibility to work autonomously, where he has shown to deliver with quality and on time the challenges we entrust him with."
+    description: "We count on Rafa's commitment and leadership, which helped us in the creation of different businesses and services that we are still running today in Desafío Latam. I would like to highlight his dynamism and responsibility to work autonomously, where he has shown to deliver with quality and on time the challenges we entrust him with."
   },
   {
     id: 2,
@@ -27,23 +28,32 @@ const references = [
     photo: person3,
     name: 'Alejandro Harcha',
     position: 'Product Manager at OOH Planning',
-    description: "Rafa has a strategic vision for product creation. By raising hypotheses and presenting the analysis for the continuous improvement of proposals, we manage to implement technological solutions and quantitatively demonstrate the success or learnings of our work. "
+    description: "Rafa has a strategic vision for product creation. By raising hypotheses and presenting the analysis for the continuous improvement of proposals, we manage to implement technological solutions and quantitatively demonstrate the success or learnings of our work."
   },
+  {
+    id: 4,
+    photo: person4,
+    name: 'Felipe Vera',
+    position: 'Front End Developer at Quiebre Marketing Inmobiliario',
+    description: "I have taken my first class with Rafael. It has been a wonderful experience where he helped me to install the necessary programs to make our first code together. You can see that he is an experienced person and the class was extremely dynamic."
+  }
 ];
 
 const ReferenceCard = ({ photo, name, position, description }) => {
   return (
-    <div className="p-6 rounded-lg shadow-lg bg-blue-500">
-      <div className="flex justify-center">
+    <div className="flex flex-col items-center bg-blue-500 rounded-lg shadow-lg p-6 text-center h-full">
+      <div className="flex justify-center mb-4">
         <img 
           src={photo} 
           alt={name} 
           className="w-24 h-24 object-cover rounded-full border-4 border-black"
         />
       </div>
-      <h3 className="mt-4 text-xl font-semibold text-white">{name}</h3>
-      <p className="mt-2 text-md font-medium text-white">{position}</p>
-      <p className="mt-2 text-base text-white">{description}</p>
+      <div className="flex flex-col flex-grow">
+        <h3 className="text-xl font-semibold text-white mb-2">{name}</h3>
+        <p className="text-md font-medium text-white mb-2">{position}</p>
+        <p className="text-base text-white">{description}</p>
+      </div>
     </div>
   );
 };
@@ -56,7 +66,7 @@ const Reference = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000, // Cambié el tiempo a 5000ms para que sea más lento
+    autoplaySpeed: 5000,
     responsive: [
       {
         breakpoint: 768,
@@ -69,21 +79,15 @@ const Reference = () => {
   };
 
   return (
-    <section className="py-12 overflow-x-hidden"> {/* overflow-x-hidden previene el desplazamiento horizontal */}
+    <section className="py-12">
       <div className="container mx-auto text-center">
         <h2 className="text-3xl font-bold text-blue-500 mb-6">
           What People Are Saying
         </h2>
         <Slider {...settings}>
           {references.map((ref) => (
-            <div key={ref.id} className="p-4">
-              <div className="flex flex-wrap -mx-4">
-                {references.slice(ref.id - 1, ref.id + 2).map((item) => (
-                  <div key={item.id} className="w-full md:w-1/2 lg:w-1/3 px-4 mb-4">
-                    <ReferenceCard {...item} />
-                  </div>
-                ))}
-              </div>
+            <div key={ref.id} className="flex justify-center">
+              <ReferenceCard {...ref} />
             </div>
           ))}
         </Slider>
