4cc0433e19e609a2b7e9cc6a673ed5e99add4e83
rquinterosv <rafael.quinterosv@gmail.com>
2024-09-05 18:30:19 +1200

    agregando google search

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,35 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import RouterComponent from './Router'; // Importa Router.jsx
+import RouterComponent from './Router';
+import { Helmet } from 'react-helmet';
 import './i18n';
 
 const App = () => {
   return (
     <Router>
+      <Helmet>
+        <title>Rafa Quinteros | Digital Transformation Expert</title>
+        <meta name="description" content="Discover the best digital transformation services and programming courses offered by Rafael Quinteros." />
+
+        
+        <meta property="og:title" content="Rafa Quinteros | Expert in Digital Transformation" />
+        <meta property="og:description" content="Programming courses and professional digital transformation services." />
+        <meta property="og:url" content="https://rafaquinterosv.com" />
+        <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/rafa-tutoring.appspot.com/o/r.png?alt=media&token=4ab2273b-3b86-454d-af10-4b02196b994d" />
+        <meta property="og:image:alt" content="Image description for accessibility" />
+        
+        
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/rafa-tutoring.appspot.com/o/r.png?alt=media&token=4ab2273b-3b86-454d-af10-4b02196b994d" />
+      </Helmet>
+
       <Navbar />
-      <div className="flex flex-col min-h-screen"> {/* Flexbox to handle the layout */}
-        <div className="flex-grow"> {/* Main content area */}
-          <RouterComponent /> {/* Usa el Router Component */}
+      <div className="flex flex-col min-h-screen"> 
+        <div className="flex-grow"> 
+          <RouterComponent /> 
         </div>
-        <Footer /> {/* Footer at the bottom */}
+        <Footer /> 
       </div>
     </Router>
   );
